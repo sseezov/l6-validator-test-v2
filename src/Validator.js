9ab44f188a0948755ff11b56b40295168f2bc0ce
sseezov <sseezov@mail.ru>
2023-12-13 13:16:06 +0700

    init

---
+++ src/Validator.js
@@ -0,0 +1,17 @@
+import ArraySchema from './ArraySchema.js';
+import NumberSchema from './NumberSchema.js';
+import ObjectSchema from './ObjectSchema.js';
+
+export default class Validator {
+  number() {
+    return new NumberSchema();
+  }
+
+  array() {
+    return new ArraySchema([(arr) => Array.isArray(arr)]);
+  }
+
+  object() {
+    return new ObjectSchema();
+  }
+}
